a5d62210b410d46f861201f6ffc1a99eeda64a99
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-05-09 15:19:10 +0300

    first

---
+++ var.js
@@ -0,0 +1,11 @@
+console.log('hi');
+let a,b=5;
+console.log(typeof a);
+a="sdxx";
+console.log(typeof b)
+
+let s1='java',s2='scrip',s3='good';
+
+let result = `lets concat -> ${s1}${s2} ${s3}`;
+
+console.log(result)
