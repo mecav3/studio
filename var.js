c2b31c4341849f7218669866ebd50240614ad631
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-05-11 01:13:40 +0300

    first

--- var.js
+++ var.js
@@ -6,6 +6,6 @@ console.log(typeof b)
 
 let s1='java',s2='scrip',s3='good';
 
-let result = `lets concat -> ${s1}${s2} ${s3}`;
+let result = `lets concat -> ${s1}${s2} is ${s3}`;
 
 console.log(result)
